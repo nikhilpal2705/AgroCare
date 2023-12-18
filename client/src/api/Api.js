e48b29c47a7fbf6615a9ec886651878bf6e60c52
Jitendra-Bakolia <bablubakolia@gmail.com>
2023-12-18 12:01:06 +0530

    Update port

--- client/src/api/Api.js
+++ client/src/api/Api.js
@@ -2,5 +2,5 @@ import axios from "axios";
 // import config from "../config/developer.config";
 
 export default axios.create({
-    baseURL: 'https://springreact-4fk2.onrender.com:8080/',
+    baseURL: 'https://springreact-4fk2.onrender.com:9000/',
 });
