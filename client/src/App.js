4f74ffb824b27c4ac43e5c53bbecec0728b2ba8a
nikhilpal2705 <nikhilpal2705@gmail.com>
2023-12-19 23:20:09 +0530

    fixed axios and readme

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,17 @@
+import api from "./api/Api"
+import { useEffect } from 'react'
 
+async function getUser(data = null) {
+  return await api.get('/getUser')
+}
 function App() {
+  useEffect(() => {
+    return async () => {
+      let response = await getUser();
+      console.log(`😱 😓 😒 ~ file: App.js:13 ~ return ~ response:`, response)
+    }
+  }, [])
+
   return (
     <div>
       <h1>Response from client</h1>
