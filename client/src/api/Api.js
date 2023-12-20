ac1f74ec70678aed324a1af4819cf51dc60d5d75
Jitendra-Bakolia <bablubakolia@gmail.com>
2023-12-20 16:01:04 +0530

    Update formData

--- client/src/api/Api.js
+++ client/src/api/Api.js
@@ -2,5 +2,5 @@ import axios from "axios";
 // import config from "../config/developer.config";
 
 export default axios.create({
-    baseURL: 'http://localhost:9000/',
+    baseURL: `http://localhost:${process.env.REACT_APP_API_PORT}/`,
 });
