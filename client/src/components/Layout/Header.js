a7be32582eebb641ed671867561a01c9f13eb560
Nikhil Suryavanshi <nikhilpal2705@gmail.com>
2024-01-01 01:09:46 +0530

    sidebar change

---
+++ client/src/components/Layout/Header.js
@@ -0,0 +1,108 @@
+import { Avatar, Dropdown, Layout } from 'antd';
+import { SettingOutlined, LogoutOutlined } from '@ant-design/icons';
+import { useNavigate, Link } from 'react-router-dom';
+
+const Header = () => {
+  const currentAdmin = {
+    name: 'Nikhil',
+    email: 'nikhil@gmail.com',
+  }
+  const { Header } = Layout;
+  const ProfileDropdown = () => {
+    const navigate = useNavigate();
+    return (
+      <div className="profileDropdown" onClick={() => navigate('/profile')}>
+        <Avatar
+          size="large"
+          className="last"
+          src={null}
+          style={{ color: '#f56a00', backgroundColor: '#f9fafc' }}
+        >
+          {currentAdmin?.name?.charAt(0)?.toUpperCase()}
+        </Avatar>
+        <div className="profileDropdownInfo">
+          <p>
+            {currentAdmin?.name}
+          </p>
+          <p>{currentAdmin?.email}</p>
+        </div>
+      </div>
+    );
+  };
+  
+  const DropdownMenu = ({ text }) => {
+    return <span style={{}}>{text}</span>;
+  };
+  const items = [
+    {
+      label: <ProfileDropdown className="headerDropDownMenu" />,
+      key: 'ProfileDropdown',
+    },
+    {
+      type: 'divider',
+    },
+    {
+      icon: <SettingOutlined />,
+      key: 'settingProfile',
+      label: (
+        <Link to={'/profile'}>
+          <DropdownMenu text="Profile" />
+        </Link>
+      ),
+    },
+    {
+      icon: <SettingOutlined />,
+      key: 'settingApp',
+      label: <Link to={'/settings'}>Settings</Link>,
+    },
+
+    {
+      type: 'divider',
+    },
+
+    {
+      icon: <LogoutOutlined />,
+      key: 'logout',
+      label: <Link to={'/logout'}>Logout</Link>,
+    },
+  ];
+  return (
+    <Header
+      style={{
+        padding: '20px',
+        background: '#f9fafc',
+        display: ' flex',
+        flexDirection: ' row-reverse',
+        justifyContent: ' flex-start',
+        gap: ' 15px',
+      }}
+    >
+      <Dropdown
+        menu={{
+          items,
+        }}
+        trigger={['click']}
+        placement="bottomRight"
+        stye={{ width: '280px', float: 'right' }}
+      >
+        <Avatar
+          className="last"
+          src={null}
+          style={{
+            color: '#f56a00',
+            backgroundColor: '#fde3cf',
+            float: 'right',
+            cursor: 'pointer',
+          }}
+          size="large"
+        >
+          {currentAdmin?.name?.charAt(0)?.toUpperCase()}
+        </Avatar>
+      </Dropdown>
+
+
+    </Header>
+  );
+};
+
+export default Header;
