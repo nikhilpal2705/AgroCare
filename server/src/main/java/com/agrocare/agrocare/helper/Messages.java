ddfa1ff4a7f2807e33cd25c1a2d1c656a11d3ed3
Jitendra-Bakolia <bablubakolia@gmail.com>
2024-01-05 22:59:36 +0530

    Crop API Done

--- server/src/main/java/com/agrocare/agrocare/helper/Messages.java
+++
@@ -1,8 +0,0 @@
-package com.agrocare.agrocare.helper;
-
-public class Messages {
-    public static final String DUPLICATE_EMAIL_MESSAGE = "Email already in use.";
-    public static final String INCORRECT_PASSWORD_MESSAGE = "Password is incorrect.";
-    public static final String REGISTRATION_SUCCESS_MESSAGE = "Registration successful! Please log in.";
-    public static final String INTERNAL_SERVER_ERROR_MESSAGE = "Internal Server Error";
-}
