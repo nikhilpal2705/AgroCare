fdea74b62b63788fcd2fe751995600d4221022ac
Jitendra-Bakolia <bablubakolia@gmail.com>
2024-01-10 16:14:05 +0530

    Fetch crop by user id

--- server/src/main/java/com/agrocare/agrocare/repository/CropRepository.java
+++ server/src/main/java/com/agrocare/agrocare/repository/CropRepository.java
@@ -4,6 +4,9 @@ import com.agrocare.agrocare.model.Crops;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CropRepository extends JpaRepository<Crops, Integer> {
+    List<Crops> findAllByUserId(int userId);
 }
