fb47fb0a917645b0b383a7792238d0da0225b83b
nikhilpal2705 <nikhilpal2705@gmail.com>
2024-01-13 21:38:26 +0530

    made frontend and backend same naming format

--- server/src/main/java/com/agrocare/agrocare/controller/common/CommonController.java
+++ server/src/main/java/com/agrocare/agrocare/controller/common/CommonController.java
@@ -1,51 +1,5 @@
 package com.agrocare.agrocare.controller.common;
 
-import com.agrocare.agrocare.helper.Constants;
-import com.agrocare.agrocare.pojo.CustomResponse;
-import com.agrocare.agrocare.model.Users;
-import com.agrocare.agrocare.service.common.CommonService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-@RestController
 public class CommonController {
-
-    private static final Logger logger = LoggerFactory.getLogger(OncePerRequestFilter.class);
-
-    @Autowired
-    private CommonService commonService;
-
-    @PostMapping(value = "/register")
-    public ResponseEntity<CustomResponse> registerUser(@RequestBody Users user) {
-        try {
-            if (commonService.existsByEmail(user.getEmail())) {
-                return new ResponseEntity<>(new CustomResponse(Constants.Messages.DUPLICATE_EMAIL),
-                        HttpStatus.BAD_REQUEST);
-            }
-            return new ResponseEntity<>(commonService.registerUser(user), HttpStatus.OK);
-        } catch (Exception err) {
-            logger.info("Error: " + err.getMessage());
-            return new ResponseEntity<>(new CustomResponse(Constants.Messages.INTERNAL_SERVER_ERROR),
-                    HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    @GetMapping(value = "/getUser", produces = "application/json")
-    public ResponseEntity<CustomResponse> getUser() {
-        try {
-            return new ResponseEntity<>(commonService.getAllUsers(), HttpStatus.OK);
-        } catch (Exception err) {
-            logger.info("Error: " + err.getMessage());
-            return new ResponseEntity<>(new CustomResponse(Constants.Messages.INTERNAL_SERVER_ERROR),
-                    HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
+    
 }
