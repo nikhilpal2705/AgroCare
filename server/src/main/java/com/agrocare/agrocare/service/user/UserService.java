4338c50d01cfe36dbc404d92e6e6be1a51805c3d
Jitendra-Bakolia <bablubakolia@gmail.com>
2024-01-18 17:50:11 +0530

    Update Password

--- server/src/main/java/com/agrocare/agrocare/service/user/UserService.java
+++ server/src/main/java/com/agrocare/agrocare/service/user/UserService.java
@@ -4,8 +4,11 @@ import com.agrocare.agrocare.helper.Constants;
 import com.agrocare.agrocare.model.Users;
 import com.agrocare.agrocare.pojo.CustomResponse;
 import com.agrocare.agrocare.repository.UserRepository;
+import com.agrocare.agrocare.service.common.CommonService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -15,6 +18,12 @@ public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private CommonService commonService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public Optional<Users> findByEmail(String email) {
         return userRepository.findByEmail(email);
@@ -28,5 +37,11 @@ public class UserService {
     public CustomResponse getUserByEmailAddress(String email) {
         return new CustomResponse(this.userRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException(Constants.Messages.USER_ID_NOT_AVAILABLE)));
+    }
+
+    public CustomResponse updateUserPassword(String password, HttpServletRequest request) {
+        Users user = commonService.getUserFromHeader(request);
+        user.setPassword(passwordEncoder.encode(password));
+        return new CustomResponse(Constants.Messages.PASSWORD_UPDATED);
     }
 }
