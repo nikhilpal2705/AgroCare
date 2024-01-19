dadb1367b18c885118cfc109e8f1805f190f9716
Jitendra-Bakolia <bablubakolia@gmail.com>
2024-01-19 18:20:02 +0530

    Dashboard

--- server/src/main/java/com/agrocare/agrocare/repository/CropRepository.java
+++ server/src/main/java/com/agrocare/agrocare/repository/CropRepository.java
@@ -1,12 +1,18 @@
 package com.agrocare.agrocare.repository;
 
 import com.agrocare.agrocare.model.Crops;
+import com.agrocare.agrocare.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Repository
 public interface CropRepository extends JpaRepository<Crops, Integer> {
     List<Crops> findAllByUserId(int userId);
+
+    Object countByUser(Users user);
+
+
 }
---
+++ server/src/main/java/com/agrocare/agrocare/service/user/DashboardService.java
@@ -0,0 +1,36 @@
+package com.agrocare.agrocare.service.user;
+
+import com.agrocare.agrocare.model.Users;
+import com.agrocare.agrocare.pojo.CustomResponse;
+import com.agrocare.agrocare.repository.CropRepository;
+import com.agrocare.agrocare.repository.PestRepository;
+import com.agrocare.agrocare.service.common.CommonService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.Serializable;
+import java.util.Map;
+
+@Service
+public class DashboardService {
+
+    @Autowired
+    private CommonService commonService;
+
+    @Autowired
+    private CropRepository cropRepository;
+
+    @Autowired
+    private PestRepository pestRepository;
+
+    public CustomResponse getDashboardDetails(HttpServletRequest request) {
+        Users user = commonService.getUserFromHeader(request);
+        Map<String, Object> response = Map.of(
+                "cropCount", this.cropRepository.countByUser(user),
+                "pestCount", this.pestRepository.countByUser(user)
+        );
+
+        return new CustomResponse(response);
+    }
+}
