d8754176dfbef1096d23169b24612f981ebfa1bb
Nikhil Suryavanshi <nikhilpal2705@gmail.com>
2024-01-22 16:31:05 +0530

    added dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,20 +1,48 @@
-import { Layout } from 'antd';
-import { Content } from 'antd/lib/layout/layout';
+import { Row, Layout } from 'antd';
+import api from 'api/api';
+import useFetch from 'hooks/useFetch';
+import SummaryCard from './SummaryCard';
 const Dashboard = () => {
+  const { result, isLoading } = useFetch(() =>
+    api.get({ entity: 'user/dashboard' })
+  );
+
+  const entityData = [
+    {
+      result: result ? result.cropCount : 0,
+      isLoading: isLoading,
+      entity: 'Crops',
+      title: 'Crops Count',
+    },
+    {
+      result: result ? result.pestCount : 0,
+      isLoading: isLoading,
+      entity: 'Pests',
+      title: 'Pests Count',
+    },
+  ];
+
+  const cards = entityData.map((data, index) => {
+    const { result, entity, isLoading } = data;
+    return (
+      <SummaryCard
+        key={index}
+        title={data?.entity}
+        tagColor={
+          data?.entity
+        }
+        prefix={'Count'}
+        isLoading={isLoading}
+        tagContent={result}
+      />
+    );
+  });
+
   return (
     <Layout className="site-layout">
-      <Content
-        className="whiteBox shadow"
-        style={{
-          padding: '50px 40px',
-          margin: '40px auto',
-          width: '100%',
-          maxWidth: '1100px',
-        }}
-      >
-        <div>Dashboard</div>
-
-      </Content>
+      <Row gutter={[32, 32]}>
+        {cards}
+      </Row>
     </Layout>
   )
 }
