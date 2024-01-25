2c3f8a9ba37e8b8e90c9acc606cd0c91ff5ce345
Jitendra-Bakolia <bablubakolia@gmail.com>
2024-01-25 22:03:09 +0530

    Irrigation impl

--- server/src/main/java/com/agrocare/agrocare/controller/user/IrrigationController.java
+++ server/src/main/java/com/agrocare/agrocare/controller/user/IrrigationController.java
@@ -1,4 +1,89 @@
 package com.agrocare.agrocare.controller.user;
 
+import com.agrocare.agrocare.helper.Constants;
+import com.agrocare.agrocare.pojo.CustomResponse;
+import com.agrocare.agrocare.pojo.InventoryRequest;
+import com.agrocare.agrocare.pojo.IrrigationRequest;
+import com.agrocare.agrocare.service.user.IrrigationService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@RestController
+@RequestMapping(value = "/user")
 public class IrrigationController {
+
+    private static final Logger logger = LoggerFactory.getLogger(OncePerRequestFilter.class);
+
+    @Autowired
+    private IrrigationService irrigationService;
+
+    // Get irrigation data list . . .
+    @GetMapping(value = "/irrigation")
+    public ResponseEntity<CustomResponse> getIrrigationList(HttpServletRequest request) {
+        try {
+            return new ResponseEntity<>(irrigationService.getIrrigationList(request), HttpStatus.OK);
+        } catch (Exception err) {
+            logger.info("Error: " + err.getMessage());
+            return new ResponseEntity<>(new CustomResponse(Constants.Messages.ERROR_WHILE_FETCHING_IRRIGATION),
+                    HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    // Get a irrigation . . .
+    @GetMapping(value = "/irrigation/{irrigationId}")
+    public ResponseEntity<CustomResponse> getIrrigation(@PathVariable("irrigationId") int irrigationId, HttpServletRequest request) {
+        try {
+            return new ResponseEntity<>(irrigationService.getIrrigation(irrigationId, request), HttpStatus.OK);
+        } catch (Exception err) {
+            logger.info("Error: " + err.getMessage());
+            return new ResponseEntity<>(new CustomResponse(Constants.Messages.ERROR_WHILE_FETCHING_IRRIGATION),
+                    HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    // Add a irrigation data . . .
+    @PostMapping(value = "/irrigation")
+    public ResponseEntity<CustomResponse> createIrrigation(@RequestBody IrrigationRequest irrigationRequest, HttpServletRequest request) {
+        try {
+            System.out.println("irrigationRequest: " + irrigationRequest);
+            return new ResponseEntity<>(irrigationService.saveIrrigation(irrigationRequest, request), HttpStatus.OK);
+        } catch (Exception err) {
+            logger.info("Error: " + err.getMessage());
+            return new ResponseEntity<>(new CustomResponse(Constants.Messages.ERROR_WHILE_ADDING_IRRIGATION),
+                    HttpStatus.BAD_REQUEST);
+        }
+    }
+
+
+    // Update a irrigation . . .
+    @PutMapping(value = "/irrigation/{irrigationId}")
+    public ResponseEntity<CustomResponse> updateIrrigation(@PathVariable("irrigationId") int irrigationId, @RequestBody IrrigationRequest irrigationRequest,
+                                                          HttpServletRequest request) {
+        try {
+            return new ResponseEntity<>(irrigationService.updateIrrigation(irrigationId, irrigationRequest, request), HttpStatus.OK);
+        } catch (Exception err) {
+            logger.info("Error: " + err.getMessage());
+            return new ResponseEntity<>(new CustomResponse(Constants.Messages.ERROR_WHILE_UPDATING_IRRIGATION_DATA),
+                    HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    // Delete a irrigation . . .
+    @DeleteMapping(value = "/irrigation/{irrigationId}")
+    public ResponseEntity<CustomResponse> deleteIrrigation(@PathVariable("irrigationId") int irrigationId, HttpServletRequest request) {
+        try {
+            return new ResponseEntity<>(irrigationService.deleteIrrigation(irrigationId, request), HttpStatus.OK);
+        } catch (Exception err) {
+            logger.info("Error: " + err.getMessage());
+            return new ResponseEntity<>(new CustomResponse(Constants.Messages.ERROR_WHILE_DELETING_IRRIGATION_DATA),
+                    HttpStatus.BAD_REQUEST);
+        }
+    }
 }
--- server/src/main/java/com/agrocare/agrocare/model/Irrigation.java
+++ server/src/main/java/com/agrocare/agrocare/model/Irrigation.java
@@ -1,4 +1,71 @@
 package com.agrocare.agrocare.model;
 
+import com.agrocare.agrocare.helper.Constants;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.util.Date;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Table(name = "irrigation")
 public class Irrigation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonProperty("id")
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "user")
+    @JsonIgnore
+    private Users user;
+
+    @ManyToOne
+    @JoinColumn(name = "crop")
+    @JsonIgnore
+    private Crops crop;
+
+    @JsonProperty("scheduleDate")
+    @Column(name = "scheduleDate", nullable = false)
+    private String scheduleDate;
+
+    @JsonProperty("status")
+    @Column(name = "status", nullable = false)
+    private int status = Constants.Status.INACTIVE;
+
+    @CreatedDate
+    @JsonProperty("createdAt")
+    @Column(name = "createdAt", nullable = false, updatable = false)
+    private String createdAt = String.valueOf(new Date().toInstant());
+
+    @LastModifiedDate
+    @JsonProperty("updatedAt")
+    @Column(name = "updatedAt", nullable = false)
+    private String updatedAt = String.valueOf(new Date().toInstant());
+
+    public Irrigation(Users user, Crops crop, String scheduleDate, int status) {
+        this.user = user;
+        this.crop = crop;
+        this.scheduleDate = scheduleDate;
+        this.status = status;
+    }
+
+    public Irrigation(int id, Users user, Crops crop, String scheduleDate, int status) {
+        this.id = id;
+        this.user = user;
+        this.crop = crop;
+        this.scheduleDate = scheduleDate;
+        this.status = status;
+    }
 }
