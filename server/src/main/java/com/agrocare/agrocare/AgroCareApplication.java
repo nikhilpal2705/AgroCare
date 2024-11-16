a43fd8e6f00a1a383f39f880e7fc0e6725b339a4
Nikhil Suryavanshi <nikhilpal2705@gmail.com>
2024-11-17 02:17:46 +0530

    managed docker

--- server/src/main/java/com/agrocare/agrocare/AgroCareApplication.java
+++ server/src/main/java/com/agrocare/agrocare/AgroCareApplication.java
@@ -3,22 +3,11 @@ package com.agrocare.agrocare;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class AgroCareApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().load();
-		// Set system properties for the properties you need
-		System.setProperty("SERVER_PORT", dotenv.get("SERVER_PORT"));
-		System.setProperty("DATABASE_NAME", dotenv.get("DATABASE_NAME"));
-		System.setProperty("DATABASE_HOST", dotenv.get("DATABASE_HOST"));
-		System.setProperty("DATABASE_USERNAME", dotenv.get("DATABASE_USERNAME"));
-		System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
-		System.setProperty("HIBERNATE_DDL", dotenv.get("HIBERNATE_DDL"));
-		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
-
 		SpringApplication.run(AgroCareApplication.class, args);
 	}
 
