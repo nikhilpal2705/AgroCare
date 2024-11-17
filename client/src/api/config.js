2d5806d385328e644bb6b69e6baaf222fcae710e
nikhilpal2705 <nikhilpal2705@gmail.com>
2024-11-17 20:30:17 +0530

    fixed dashboard api issue

--- client/src/api/config.js
+++ client/src/api/config.js
@@ -1,7 +1,9 @@
-export const BASE_URL = process.env.REACT_APP_SERVER_BASE_URL
-export const HOME_BASE_URL = BASE_URL + "/"
-export const AUTH_BASE_URL = BASE_URL + "/auth/"
-export const ADMIN_BASE_URL = BASE_URL + "/admin/"
-export const USER_BASE_URL = BASE_URL + "/user/"
-export const COMMON_BASE_URL = BASE_URL + "/common/"
+export const BASE_URL = window.location.origin
+export const API_BASE_URL = BASE_URL + "/api"
+console.log(`😱 😓 😒 ~ API_BASE_URL:`, API_BASE_URL)
+export const HOME_BASE_URL = API_BASE_URL + "/"
+export const AUTH_BASE_URL = API_BASE_URL + "/auth/"
+export const ADMIN_BASE_URL = API_BASE_URL + "/admin/"
+export const USER_BASE_URL = API_BASE_URL + "/user/"
+export const COMMON_BASE_URL = API_BASE_URL + "/common/"
 export const ACCESS_TOKEN_NAME = "Bearer ";
