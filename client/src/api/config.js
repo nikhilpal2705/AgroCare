4eaedc0ea9b5783fb27aacf68cbe4e29ab383e2e
Nikhil Suryavanshi <nikhilpal2705@gmail.com>
2024-11-17 21:20:09 +0530

    removed console

--- client/src/api/config.js
+++ client/src/api/config.js
@@ -1,6 +1,5 @@
-export const BASE_URL = window.location.origin
-export const API_BASE_URL = BASE_URL + "/api"
-console.log(`😱 😓 😒 ~ API_BASE_URL:`, API_BASE_URL)
+export const BASE_URL = process.env.NODE_ENV === 'development' ? process.env.REACT_APP_SERVER_BASE_URL : window.location.origin
+export const API_BASE_URL = process.env.NODE_ENV === 'development' ? BASE_URL : BASE_URL + "/api"
 export const HOME_BASE_URL = API_BASE_URL + "/"
 export const AUTH_BASE_URL = API_BASE_URL + "/auth/"
 export const ADMIN_BASE_URL = API_BASE_URL + "/admin/"
